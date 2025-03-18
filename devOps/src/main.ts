ceed6cd8c1a486dac168e7f075313f3864cab3a8
Dasha <girl.is.anime20@gmail.com>
2025-03-18 14:26:35 +0300

    feat: add main image in project

--- devOps/src/main.ts
+++ devOps/src/main.ts
@@ -1,13 +1,19 @@
 import { Header } from "./header/Header";
 import { Form } from "./form/Form";
+import { Img } from "./img/Img";
 
 export class App {
   private header: Header;
   private form: Form;
+  private img: Img
 
   constructor(private container: HTMLElement) {
     this.header = new Header();
     this.form = new Form();
+    this.img = new Img(
+      '/./main-dog.png',
+      'Logo'
+    );
     this.init();
   }
 
@@ -17,6 +23,9 @@ export class App {
 
     const formElement = this.form.getFormElement();
     this.container.append(formElement);
+
+    const imgContainer = this.img.getImageElement();
+    this.container.append(imgContainer);
   }
 
 }
