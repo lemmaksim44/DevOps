d1881cd10cf7fe28870ba747d1c2231ed6c8221b
Dasha <girl.is.anime20@gmail.com>
2025-03-18 14:10:02 +0300

    feat: add form element in project

---
+++ devOps/src/form/Form.ts
@@ -0,0 +1,59 @@
+// @ts-ignore
+import './Form.css';
+
+export class Form {
+  private formContainer: HTMLElement;
+
+  constructor() {
+    this.formContainer = this.createForm();
+  }
+
+  private createForm(): HTMLElement {
+    const form = document.createElement('form');
+    form.classList.add('form-container');
+
+    const loginField = this.createInputField('login', 'text', 'Login');
+    form.append(loginField);
+
+    const passwordField = this.createInputField('password', 'password', 'Password');
+    form.append(passwordField);
+
+    const submitBtn = this.createSubmitBtn('Enter');
+    form.append(submitBtn);
+
+    return form;
+  }
+
+  private createInputField(id: string, type: string, labelText: string): HTMLElement {
+    const container = document.createElement('div');
+    container.classList.add('form-group');
+
+    const label = document.createElement('label');
+    label.setAttribute('for', id);
+    label.textContent = labelText;
+
+    const input = document.createElement('input');
+    input.setAttribute('type', type);
+    input.setAttribute('id', id);
+    input.setAttribute('name', id);
+    input.setAttribute('placeholder', `Введите ${labelText.toLowerCase()}`);
+
+    container.append(label);
+    container.append(input);
+
+    return container;
+  }
+
+  private createSubmitBtn(text: string): HTMLElement {
+    const btnSubmit = document.createElement('button');
+    btnSubmit.setAttribute('type', 'submit');
+    btnSubmit.classList.add('submit-btn');
+    btnSubmit.textContent = text;
+
+    return btnSubmit;
+  }
+
+  public getFormElement(): HTMLElement {
+    return this.formContainer;
+  }
+}
--- devOps/src/main.ts
+++ devOps/src/main.ts
@@ -1,16 +1,22 @@
 import { Header } from "./header/Header";
+import { Form } from "./form/Form";
 
 export class App {
   private header: Header;
+  private form: Form;
 
   constructor(private container: HTMLElement) {
     this.header = new Header();
+    this.form = new Form();
     this.init();
   }
 
   private init(): void {
     const headerElement = this.header.getHeaderElement();
-    this.container.appendChild(headerElement);
+    this.container.append(headerElement);
+
+    const formElement = this.form.getFormElement();
+    this.container.append(formElement);
   }
 
 }
