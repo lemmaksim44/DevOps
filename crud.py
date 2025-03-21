c22aad134b2124632ed3772bba24787a0e8ae690
Maksim Lezhankov <lemmaksim44@gmail.com>
2025-03-21 11:33:28 +0300

    feat: add authorization and token verification

--- crud.py
+++ crud.py
@@ -1,6 +1,12 @@
 from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
 import bcrypt
+import jwt
+from jwt import PyJWTError
 
+from database import get_db
 from models import User
 from schemas import *
 
@@ -17,5 +23,47 @@ def create_user(user: RegUser, db: Session):
     return new_user
 
 
-def get_user(user: GetUser, db: Session):
-    return db.query(User).filter(User.username == user.username).first()
+def get_user(username, db: Session):
+    return db.query(User).filter(User.username == username).first()
+
+
+def auth_user(user: AuthUser, db: Session):
+    db_user = get_user(user.username, db)
+    if db_user and bcrypt.checkpw(user.password.encode("utf-8"), db_user.password.encode("utf-8")):
+        return db_user
+    return None
+
+
+SECRET_KEY = "VetKey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(status_code=401, detail="Could not validate credentials")
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+
+    user = get_user(username, db)
+    if user is None:
+        raise credentials_exception
+
+    return user
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def root():
 
 @app.post("/register/")
 async def register(user: RegUser, db: Session = Depends(get_db)):
-    db_user = get_user(user, db)
+    db_user = get_user(user.username, db)
     if db_user:
         raise HTTPException(status_code=400, detail="User already exists")
 
@@ -24,3 +24,18 @@ async def register(user: RegUser, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Weak password")
 
     return create_user(user, db)
+
+
+@app.post("/auth/")
+async def auth(user: AuthUser, db: Session = Depends(get_db)):
+    db_user = auth_user(user, db)
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    access_token = create_access_token(data={"sub": db_user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/auth/", response_model=GetUser)
+async def status_auth(user: GetUser = Depends(get_current_user)):
+    return user
